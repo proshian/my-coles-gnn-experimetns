fc81ed9abcb93a89a09afb7436cdec4caa0e3287
Garri Proshian <proshian.itmo@gmail.com>
2024-06-27 21:49:29 +0300

    Delete everything unused

--- scenario_rosbank/bin/get-data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir data
-cd data
-
-curl -OL 'https://huggingface.co/datasets/dllllb/rosbank-churn/resolve/main/train.csv.gz?download=true'
-curl -OL 'https://huggingface.co/datasets/dllllb/rosbank-churn/resolve/main/test.csv.gz?download=true'
-
-gunzip -f *.csv.gz
