7ec6b75588e0f5d0b3a0a67126b16c81a390f930
Garry Proshian <proshian.itmo@gmail.com>
2024-06-28 14:49:53 +0300

    update configs

--- scenario_age_pred/bin/scenario_coles_gnn__for_check.sh
+++ scenario_age_pred/bin/scenario_coles_gnn__for_check.sh
@@ -4,14 +4,18 @@
 
 # PYTHONPATH is set to make coles_and_gnn_frame available
 PYTHONPATH=.. python -m pl_train_module \
-    --config-dir conf --config-name coles_gnn_params__for_check.yaml \
+    --config-dir conf --config-name coles_gnn_params \
     data_module.train_data.splitter.split_count=2 \
     data_module.valid_data.splitter.split_count=2 \
     pl_module.validation_metric.K=1 \
     trainer.max_epochs=1 \
     pl_module.lr_scheduler_partial.step_size=60 \
     model_path="models/mles_model2.p" \
-    logger_name="mles_model2" 
+    logger_name="mles_model2"  \
+    data_module.train_batch_size=32 \
+    data_module.train_num_workers=4 \
+    data_module.valid_batch_size=48 \
+    data_module.valid_num_workers=4
     
 PYTHONPATH=.. python -m pl_inference    \
     model_path="models/mles_model2.p" \
