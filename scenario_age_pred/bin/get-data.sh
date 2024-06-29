3ea44c620b523c2c6046117bfa006c1a1c927504
Garry Proshian <proshian.itmo@gmail.com>
2024-06-29 03:04:58 +0300

    fix get-data

--- scenario_age_pred/bin/get-data.sh
+++ scenario_age_pred/bin/get-data.sh
@@ -3,8 +3,8 @@
 mkdir data
 cd data
 
-curl -OL 'https://huggingface.co/datasets/dllllb/age-group-prediction/resolve/main/transactions_train.csv.gz?download=true'
-curl -OL 'https://huggingface.co/datasets/dllllb/age-group-prediction/resolve/main/transactions_test.csv.gz?download=true'
-curl -OL 'https://huggingface.co/datasets/dllllb/age-group-prediction/resolve/main/train_target.csv?download=true'
+curl -OL 'https://huggingface.co/datasets/dllllb/age-group-prediction/resolve/main/transactions_train.csv.gz'
+curl -OL 'https://huggingface.co/datasets/dllllb/age-group-prediction/resolve/main/transactions_test.csv.gz'
+curl -OL 'https://huggingface.co/datasets/dllllb/age-group-prediction/resolve/main/train_target.csv'
 
 gunzip -f *.csv.gz
