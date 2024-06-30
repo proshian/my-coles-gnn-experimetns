b8557bcea83b50d91235d4f37194515b0377dfec
Garry Proshian <proshian.itmo@gmail.com>
2024-07-01 00:18:39 +0300

    Update age scripts

--- scenario_age_pred/bin/scenario_coles__small_batch.sh
+++ scenario_age_pred/bin/scenario_coles__small_batch.sh
@@ -6,16 +6,18 @@ python -m ptls.pl_train_module \
     data_module.valid_data.splitter.split_count=2 \
     pl_module.validation_metric.K=1 \
     pl_module.lr_scheduler_partial.step_size=60 \
-    model_path="models/mles_model_2.p" \
-    logger_name="mles_model_2" \
+    model_path="models/mles_model_small_batch_2.p" \
+    logger_name="mles_model_small_batch_2" \
     data_module.train_batch_size=32 \
     data_module.train_num_workers=4 \
     data_module.valid_batch_size=48 \
     data_module.valid_num_workers=4 \
     trainer.max_epochs=300 \  # trainer.max_epochs=1 \
 
+
 python -m ptls.pl_inference    \
-    model_path="models/mles_model_2.p" \
-    embed_file_name="mles_2_embeddings" \
+    model_path="models/mles_model_small_batch_2.p" \
+    embed_file_name="mles_model_small_batch_2_embeddings" \
     inference.batch_size=40 \
-    --config-dir conf --config-name mles_params
+    --config-dir conf --config-name mles_params 
+    
--- scenario_age_pred/bin/scenario_coles_gnn__for_check.sh
+++ scenario_age_pred/bin/scenario_coles_gnn__for_check.sh
@@ -15,7 +15,7 @@ PYTHONPATH=.. python -m ptls.pl_train_module \
     data_module.train_num_workers=4 \
     data_module.valid_batch_size=48 \
     data_module.valid_num_workers=4  \
-    trainer.max_epochs=300 \  # trainer.max_epochs=300 \
+    trainer.max_epochs=1 \
     
 # PYTHONPATH=.. python -m ptls.pl_inference    \
 #     model_path="models/coles_gnn_model_for_check_2.p" \
