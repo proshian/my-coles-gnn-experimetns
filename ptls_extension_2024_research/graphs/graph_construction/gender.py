78a31bd0a176d72a2a25840eca849b0951a43aba
Nikis14 <severin.nikita@mail.ru>
2024-08-03 01:32:51 +0400

    Added correct weights consideration

--- ptls_extension_2024_research/graphs/graph_construction/gender.py
+++ ptls_extension_2024_research/graphs/graph_construction/gender.py
@@ -11,31 +11,23 @@ class GenderGraphBuilder(GraphBuilder):
         return df
 
     def _build_weighted_edge_df(self, df, client_col, item_col):
-        # df = df[[client_col, item_col, 'amount']].copy()
         df = df[[client_col, item_col, 'amount']]
-        df = self.preprocess(df)
-        df_type = df.groupby([client_col, item_col]).agg(sum).reset_index()
-        df_type = df_type.rename({'amount': 'amount_type_mcc'}, axis=1)
-        df_cl = df[[client_col, 'amount']].groupby([client_col]).agg(sum).reset_index()
-        df_cl = df_cl.rename({'amount': 'amount_client_total'}, axis=1)
-        df_merged = df_type.merge(df_cl, on=client_col, how='left')
-        df_merged['amount_tf'] = df_merged['amount_type_mcc'] / df_merged['amount_client_total']
+        df['amount'] = df['amount'].apply(abs)
+        df['amount'] = np.log1p(abs(df['amount'])) * np.sign(df['amount'])
 
-        df_mean_mcc_type = df[[item_col, 'amount']].groupby([item_col]).agg('mean').reset_index()
-        df_mean_mcc_type['amount'] = np.sqrt(df_mean_mcc_type['amount'].sum() / df_mean_mcc_type['amount'])
-        df_mean_mcc_type = df_mean_mcc_type.rename({'amount': 'mcc_type_norm'}, axis=1)
+        grouped_edges = df.groupby([client_col, item_col]).agg(sum)
+        edge2sum_amount = dict(zip(grouped_edges.index, grouped_edges['amount']))
 
-        df_merged = df_merged.merge(df_mean_mcc_type, on=[item_col], how='left')
-        df_merged['amount_tf'] = df_merged['amount_tf'] * df_merged['mcc_type_norm']
+        grouped_item_weights = df.groupby([item_col]).agg(sum)
+        item2sum = dict(zip(grouped_item_weights.index, grouped_item_weights['amount']))
 
-        unique_clients_cnt = len(df[client_col].unique())
-        df_client_cnt = df[[client_col, item_col]].drop_duplicates().groupby([item_col]).agg(len).reset_index()
-        df_client_cnt = df_client_cnt.rename({client_col: 'unique_customers'}, axis=1)
-        df_client_cnt['amount_idf'] = np.log(unique_clients_cnt / df_client_cnt['unique_customers'])
-        df_merged = df_merged.merge(df_client_cnt, on=[item_col], how='left')
-        df_merged['weight'] = df_merged['amount_tf'] * df_merged['amount_idf']
-        assert all(df_merged['weight'] != 0)
-        return df_merged, client_col, item_col, 'weight'
+        df_total = df[[client_col, item_col]].drop_duplicates()
+
+        df_total['weight'] = df_total.apply(lambda row:
+                                            edge2sum_amount[(row['customer_id'], row['mcc_code'])] / item2sum[
+                                                row['mcc_code']], axis=1)
+        assert all(df_total['weight'] != 0)
+        return df_total, client_col, item_col, 'weight'
 
     def _build_simple_edge_df(self, df, client_col, item_col):
         return df.drop_duplicates([client_col, item_col]), client_col, item_col
--- ptls_extension_2024_research/graphs/static_models/gnn.py
+++ ptls_extension_2024_research/graphs/static_models/gnn.py
@@ -3,38 +3,41 @@ import torch
 import torch.nn as nn
 
 class GraphSAGE(nn.Module):
-    def __init__(self, in_feats, h_feats, num_layers):
+    def __init__(self, in_feats, h_feats, use_edge_weights, num_layers):
         super(GraphSAGE, self).__init__()
         self.layers = nn.ModuleList()
         self.layers.append(SAGEConv(in_feats, h_feats, 'mean'))
+        self.use_edge_weights = use_edge_weights
         for _ in range(num_layers):
             self.layers.append(SAGEConv(h_feats, h_feats, 'mean'))
 
-    def forward(self, g, in_feat):
+    def forward(self, g, in_feat, edge_weights):
         h = in_feat
         for layer in self.layers[:-1]:
-            h = layer(g, h)
+            h = layer(g, h, edge_weight=edge_weights)
             h = torch.relu(h)
-        h = self.layers[-1](g, h)
+        h = self.layers[-1](g, h, edge_weights)
         return h
 
 
 class GAT(nn.Module):
-    def __init__(self, in_feats, h_feats, num_heads, num_layers,
+    def __init__(self, in_feats, h_feats, use_edge_weights, num_heads, num_layers,
                  feat_drop=0.6, attn_drop=0.6):
         super(GAT, self).__init__()
         self.layers = nn.ModuleList()
         assert h_feats % num_heads == 0
         self.layer_hfeats = h_feats // num_heads
+        self.use_edge_weights = use_edge_weights
         self.layers.append(GATConv(in_feats, self.layer_hfeats, num_heads, feat_drop=feat_drop, attn_drop=attn_drop, activation=nn.ELU()))
         for _ in range(num_layers - 1):
             self.layers.append(GATConv(h_feats, self.layer_hfeats, num_heads, feat_drop=feat_drop, attn_drop=attn_drop, activation=nn.ELU()))
 
-    def forward(self, g, in_feat):
+    def forward(self, g, in_feat, edge_weights=None):
         h = in_feat
         for layer in self.layers[:-1]:
-            h = layer(g, h)
+            h = layer(g, h, edge_weight=edge_weights)
             h = h.flatten(1)
-        h = self.layers[-1](g, h)
+        h = self.layers[-1](g, h, edge_weight=edge_weights)
+        h = h.flatten(1)
         return h
         # return h.mean(1)
